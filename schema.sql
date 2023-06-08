c0404de1e5c3cbd59dfd60acb00ddef0ce92b2a6
Juan Sanchez <juansan1993@hotmail.com>
2023-06-08 17:38:55 -0500

    Update schema

--- schema.sql
+++ schema.sql
@@ -34,4 +34,25 @@ ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owner_id INT REFERENCES owners(id);
 
+CREATE TABLE vets(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+);
 
+CREATE TABLE specializations(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    vet_id INT REFERENCES vets(id),
+	species_id INT REFERENCES species(id),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE visits(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	animal_id INT REFERENCES animals(id),
+	vet_id INT REFERENCES vets(id),
+	visit_date DATE,
+	PRIMARY KEY(id)
+);
